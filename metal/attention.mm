1ae62b160a7dc977c099640cf229ed7800e2bd57
0xaaaaaron <c@crynux.ai>
2025-03-04 00:39:51 -0800

    remove useless autoreleasepool

--- metal/attention.mm
+++ metal/attention.mm
@@ -24,88 +24,86 @@ Tensor Attention<BackendType::METAL>::forward(
     Tensor result({batch, seqlen, _dim});
 
 
-    @autoreleasepool {
-        // wq(input), wk(input), wv(input)
-        int params[] = {batch, seqlen, _max_seq_len, _dim, start_pos};
-        _executor->addBuffer(obj_id, Attention_INPUT, input._value.get(), inputByteSize);
-        _executor->addBuffer(obj_id, Attention_XQ, xq._value.get(), inputByteSize);
-        _executor->addBuffer(obj_id, Attention_X_PARAMS, params, 5*sizeof(int));
-        _executor->forward(obj_id, 3,
-            {
-                Attention_X_PARAMS,
-                Attention_INPUT,
-                Attention_WEIGHT_Q,
-                Attention_WEIGHT_K,
-                Attention_WEIGHT_V,
-                Attention_XQ,
-                Attention_CACHE_K,
-                Attention_CACHE_V,
-            },
-            {batch, seqlen, _dim});
+    // wq(input), wk(input), wv(input)
+    int params[] = {batch, seqlen, _max_seq_len, _dim, start_pos};
+    _executor->addBuffer(obj_id, Attention_INPUT, input._value.get(), inputByteSize);
+    _executor->addBuffer(obj_id, Attention_XQ, xq._value.get(), inputByteSize);
+    _executor->addBuffer(obj_id, Attention_X_PARAMS, params, 5*sizeof(int));
+    _executor->forward(obj_id, 3,
+        {
+            Attention_X_PARAMS,
+            Attention_INPUT,
+            Attention_WEIGHT_Q,
+            Attention_WEIGHT_K,
+            Attention_WEIGHT_V,
+            Attention_XQ,
+            Attention_CACHE_K,
+            Attention_CACHE_V,
+        },
+        {batch, seqlen, _dim});
 
-        // Rope apply_rotary_emb
-        int rope_params[] = {batch, _max_seq_len, seqlen, start_pos, _dim, _num_heads, num_complex};
-        _executor->addBuffer(obj_id, Attention_ROPE_PARAMS, rope_params, 7*sizeof(float));
-        _executor->addBuffer(obj_id, Attention_ROPE_COST, rope_cost._value.get(), ropeByteSize);
-        _executor->addBuffer(obj_id, Attention_ROPE_SINT, rope_sint._value.get(), ropeByteSize);
-        _executor->forward(obj_id, 4,
-            {
-                Attention_ROPE_PARAMS,
-                Attention_ROPE_COST,
-                Attention_ROPE_SINT,
-                Attention_XQ,
-                Attention_CACHE_K,
-            },
-            {batch, seqlen, _dim / 2});
+    // Rope apply_rotary_emb
+    int rope_params[] = {batch, _max_seq_len, seqlen, start_pos, _dim, _num_heads, num_complex};
+    _executor->addBuffer(obj_id, Attention_ROPE_PARAMS, rope_params, 7*sizeof(float));
+    _executor->addBuffer(obj_id, Attention_ROPE_COST, rope_cost._value.get(), ropeByteSize);
+    _executor->addBuffer(obj_id, Attention_ROPE_SINT, rope_sint._value.get(), ropeByteSize);
+    _executor->forward(obj_id, 4,
+        {
+            Attention_ROPE_PARAMS,
+            Attention_ROPE_COST,
+            Attention_ROPE_SINT,
+            Attention_XQ,
+            Attention_CACHE_K,
+        },
+        {batch, seqlen, _dim / 2});
 
 
-        // softmax(QK^T/scale)) (not averaged)
-        int compute_score_param[] = {batch, _max_seq_len, seqlen, start_pos, _dim, _num_heads, mask ? 1 : 0};
-        _executor->addBuffer(obj_id, Attention_SCORE_PARAMS, compute_score_param, 7*sizeof(float));
-        _executor->addBuffer(obj_id, Attention_SCORE, score._value.get(), scoreByteSize);
-        _executor->forward(obj_id, 5,
-            {
-                Attention_SCORE_PARAMS,
-                Attention_XQ,
-                Attention_CACHE_K,
-                Attention_CACHE_V,
-                Attention_SCORE,
-            },
-            {batch, seqlen, totlen * _num_heads});
+    // softmax(QK^T/scale)) (not averaged)
+    int compute_score_param[] = {batch, _max_seq_len, seqlen, start_pos, _dim, _num_heads, mask ? 1 : 0};
+    _executor->addBuffer(obj_id, Attention_SCORE_PARAMS, compute_score_param, 7*sizeof(float));
+    _executor->addBuffer(obj_id, Attention_SCORE, score._value.get(), scoreByteSize);
+    _executor->forward(obj_id, 5,
+        {
+            Attention_SCORE_PARAMS,
+            Attention_XQ,
+            Attention_CACHE_K,
+            Attention_CACHE_V,
+            Attention_SCORE,
+        },
+        {batch, seqlen, totlen * _num_heads});
 
-        // score @ cachev
-        _executor->addBuffer(obj_id, Attention_OUTPUT, output._value.get(), inputByteSize);
-        _executor->forward(obj_id, 6,
-            {
-                Attention_SCORE_PARAMS,
-                Attention_SCORE,
-                Attention_CACHE_V,
-                Attention_OUTPUT,
-            },
-            {batch, seqlen, _dim});
+    // score @ cachev
+    _executor->addBuffer(obj_id, Attention_OUTPUT, output._value.get(), inputByteSize);
+    _executor->forward(obj_id, 6,
+        {
+            Attention_SCORE_PARAMS,
+            Attention_SCORE,
+            Attention_CACHE_V,
+            Attention_OUTPUT,
+        },
+        {batch, seqlen, _dim});
 
-        // Wo @ output
-        int result_param[] = {batch, _max_seq_len, seqlen, start_pos, _dim, _num_heads, residual ? 1 : 0};
-        _executor->addBuffer(obj_id, Attention_RESULT_PARAMS, result_param, 7*sizeof(float));
-        _executor->addBuffer(obj_id, Attention_RESULT, result._value.get(), inputByteSize);
-        int residual_idx = Attention_INPUT;   // dummy input
-        if (residual) {
-            _executor->addBuffer(obj_id, Attention_RESIDUAL, residual->_value.get(), inputByteSize);
-            residual_idx = Attention_RESIDUAL;
-        }
-        _executor->forward(obj_id, 7,
-            {
-                Attention_RESULT_PARAMS,
-                Attention_OUTPUT,
-                Attention_WEIGHT_O,
-                residual_idx,
-                Attention_RESULT,
-            },
-            {batch, seqlen, _dim});
-        
-        // Convert result
-        _executor->bufferToTensor(obj_id, Attention_RESULT, &result);
-
-        return result;
+    // Wo @ output
+    int result_param[] = {batch, _max_seq_len, seqlen, start_pos, _dim, _num_heads, residual ? 1 : 0};
+    _executor->addBuffer(obj_id, Attention_RESULT_PARAMS, result_param, 7*sizeof(float));
+    _executor->addBuffer(obj_id, Attention_RESULT, result._value.get(), inputByteSize);
+    int residual_idx = Attention_INPUT;   // dummy input
+    if (residual) {
+        _executor->addBuffer(obj_id, Attention_RESIDUAL, residual->_value.get(), inputByteSize);
+        residual_idx = Attention_RESIDUAL;
     }
+    _executor->forward(obj_id, 7,
+        {
+            Attention_RESULT_PARAMS,
+            Attention_OUTPUT,
+            Attention_WEIGHT_O,
+            residual_idx,
+            Attention_RESULT,
+        },
+        {batch, seqlen, _dim});
+    
+    // Convert result
+    _executor->bufferToTensor(obj_id, Attention_RESULT, &result);
+
+    return result;
 }
--- metal/feedforward.mm
+++ metal/feedforward.mm
@@ -9,52 +9,50 @@ Tensor FFNSwiGLU<BackendType::METAL>::forward(const Tensor& input, Tensor* resid
     int seqlen = input.shape()[1];
     assert(input.shape()[2] == _dim);
 
-    @autoreleasepool {
-        size_t inputByteSize = batch * seqlen * _dim * sizeof(float);
-        size_t wByteSize = _dim * _hidden_dim * sizeof(float);
-        size_t resByteSize = batch * seqlen * _hidden_dim * sizeof(float);
+    size_t inputByteSize = batch * seqlen * _dim * sizeof(float);
+    size_t wByteSize = _dim * _hidden_dim * sizeof(float);
+    size_t resByteSize = batch * seqlen * _hidden_dim * sizeof(float);
 
-        Tensor hidden_output({batch, seqlen, _hidden_dim});
-        Tensor result(input.shape());
+    Tensor hidden_output({batch, seqlen, _hidden_dim});
+    Tensor result(input.shape());
 
-        // silu(w1(x)) * w3(x)
-        int params[] = {batch, seqlen, _dim, _hidden_dim};
-        _executor->addBuffer(obj_id, FFNSwiGLU_PARAM_1, params, 4*sizeof(int));
-        _executor->addBuffer(obj_id, FFNSwiGLU_INPUT, input._value.get(), inputByteSize);
-        _executor->addBuffer(obj_id, FFNSwiGLU_HIDDEN_OUTPUT, hidden_output._value.get(), resByteSize);
+    // silu(w1(x)) * w3(x)
+    int params[] = {batch, seqlen, _dim, _hidden_dim};
+    _executor->addBuffer(obj_id, FFNSwiGLU_PARAM_1, params, 4*sizeof(int));
+    _executor->addBuffer(obj_id, FFNSwiGLU_INPUT, input._value.get(), inputByteSize);
+    _executor->addBuffer(obj_id, FFNSwiGLU_HIDDEN_OUTPUT, hidden_output._value.get(), resByteSize);
 
-        _executor->forward(obj_id, 1,
-            {
-                FFNSwiGLU_PARAM_1,
-                FFNSwiGLU_INPUT,
-                FFNSwiGLU_W1,
-                FFNSwiGLU_W3,
-                FFNSwiGLU_HIDDEN_OUTPUT,
-            },
-            {batch, seqlen, _hidden_dim});
+    _executor->forward(obj_id, 1,
+        {
+            FFNSwiGLU_PARAM_1,
+            FFNSwiGLU_INPUT,
+            FFNSwiGLU_W1,
+            FFNSwiGLU_W3,
+            FFNSwiGLU_HIDDEN_OUTPUT,
+        },
+        {batch, seqlen, _hidden_dim});
 
-        // w2(res)
-        int params2[] = {batch, seqlen, _dim, _hidden_dim, residual ? 1 : 0};
-        _executor->addBuffer(obj_id, FFNSwiGLU_PARAM_2, params2, 5*sizeof(int));
-        _executor->addBuffer(obj_id, FFNSwiGLU_RESULT, result._value.get(), inputByteSize);
-        int residual_idx = FFNSwiGLU_INPUT;  // Dummy input.
-        if (residual) {
-            _executor->addBuffer(obj_id, FFNSwiGLU_RESIDUAL, residual->_value.get(), inputByteSize);
-            residual_idx = FFNSwiGLU_RESIDUAL;
-        }
-
-        _executor->forward(obj_id, 2,
-            {
-                FFNSwiGLU_PARAM_2,
-                residual_idx,
-                FFNSwiGLU_W2,
-                FFNSwiGLU_HIDDEN_OUTPUT,
-                FFNSwiGLU_RESULT,
-            },
-            {batch, seqlen, _dim});
-
-        _executor->bufferToTensor(obj_id, FFNSwiGLU_RESULT, &result);
-        return result;
+    // w2(res)
+    int params2[] = {batch, seqlen, _dim, _hidden_dim, residual ? 1 : 0};
+    _executor->addBuffer(obj_id, FFNSwiGLU_PARAM_2, params2, 5*sizeof(int));
+    _executor->addBuffer(obj_id, FFNSwiGLU_RESULT, result._value.get(), inputByteSize);
+    int residual_idx = FFNSwiGLU_INPUT;  // Dummy input.
+    if (residual) {
+        _executor->addBuffer(obj_id, FFNSwiGLU_RESIDUAL, residual->_value.get(), inputByteSize);
+        residual_idx = FFNSwiGLU_RESIDUAL;
     }
+
+    _executor->forward(obj_id, 2,
+        {
+            FFNSwiGLU_PARAM_2,
+            residual_idx,
+            FFNSwiGLU_W2,
+            FFNSwiGLU_HIDDEN_OUTPUT,
+            FFNSwiGLU_RESULT,
+        },
+        {batch, seqlen, _dim});
+
+    _executor->bufferToTensor(obj_id, FFNSwiGLU_RESULT, &result);
+    return result;
     
 }
